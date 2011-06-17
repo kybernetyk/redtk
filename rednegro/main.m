3254053563dd96d1ea30c6781b690666d499f3d8
jsz <jszpilewski@me.com>
2011-06-17 19:42:04 +0200

    Initial Commit

---
+++ rednegro/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  rednegro
+//
+//  Created by jrk on 17/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
