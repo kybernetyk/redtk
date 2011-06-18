068f0c9c4af19d2d8700bca4fbdc89b32c4f7196
jsz <jszpilewski@me.com>
2011-06-18 18:29:24 +0200

    nyancat

---
+++ redtoolkit/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  redtoolkit
+//
+//  Created by jrk on 18/6/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
